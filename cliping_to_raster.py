fbc41c65ade51241b52c73ede264cb94982357f4
Vance Harris <53877381+harrisv25@users.noreply.github.com>
2020-05-14 12:47:45 -0600

    Update cliping_to_raster.py

--- cliping_to_raster.py
+++ cliping_to_raster.py
@@ -4,40 +4,44 @@ import fiona
 from shapely.geometry import mapping, shape
 from osgeo import ogr, osr, gdal
 
-os.chdir(r"C:\Users\harri_000\Documents\Mongolia\harris_package\harris_package")
+os.chdir("your directory")
 
-shp48=r'GPS_Points\points_48.shp'
-shp49=r'GPS_Points\points_49.shp'
+#my images cover two seperate projections, so the a shapefile was created for each projection. 
+shp48=r'points_48.shp'
+shp49=r'points_49.shp'
 
-r1=r"images\Index of 011315448020_01\011315448020_01_P001_MUL\19SEP25034353-M2AS-011315448020_01_P001.tif"
-r2=r"images\Index of 011315448030_01\011315448030_01_P001_MUL\19SEP08040927-M2AS-011315448030_01_P001.tif"
-r3=r"images\Index of 011315448040_01\011315448040_01_P001_MUL\19SEP08040926-M2AS-011315448040_01_P001.tif"
+r1=r"image 1"
+r2=r"image 2"
+r3=r"image 3"
 
 rasters=[r1,r2,r3]
 
-
+#the intent is to just use points and a raster.Everything else will be automated in the function
 
 def clip_v_to_r(shp, rast):
     
 # First we will open our raster image, to understand how we will want to rasterize our vector
     src=rasterio.open(rast)
     ext=src.bounds
-
+#the left, right, top bottom coordinates will be used to draw a bounding box around the points
     xmin = ext[0]
     xmax = ext[2]
     ymin = ext[1]
     ymax = ext[3]
     src.close()
+    #create an empty list that the points within the box will be written into
     poly = []
     with fiona.open(shp) as c:
         for pol in c:
             geom = shape(pol['geometry'])
+            #check to see if the geometry opened is within the box. If so it is appended to the list.
             if geom.coords[0][0] >= xmin and geom.coords[0][0] <= xmax:
                 if geom.coords[0][1] >= ymin and geom.coords[0][1] <= ymax:
                     poly.append([geom, pol])
         
     print(ext,len(poly))
-# Here's an example Shapely geometry
+# The schema is based ioff the original shapefile's attribute table. NOthing needs
+    #to be altered, simplpy copied to a new shapefile
     myschema =  {'geometry': 'Point',
                  'properties': {'Location': 'str',
                                 'Type': 'str',
